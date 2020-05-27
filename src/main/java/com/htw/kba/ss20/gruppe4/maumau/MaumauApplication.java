9edfe8f83a1a5462e0ceb3bf331298cccb4515f5
FelixOhlhof <Fecati97>
2020-05-27 17:18:00 +0200

    Start

---
+++ src/main/java/com/htw/kba/ss20/gruppe4/maumau/MaumauApplication.java
@@ -0,0 +1,14 @@
+package com.htw.kba.ss20.gruppe4.maumau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaumauApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MaumauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/htw/kba/ss20/gruppe4/maumau/MaumauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htw.kba.ss20.gruppe4.maumau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaumauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
